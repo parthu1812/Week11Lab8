6a2674098fff25d169cb83970d1d4d2995bf064c
PARTHU <PARTHU@LAPTOP-P6816BHV>
2024-04-03 22:40:25 -0400

    Add project files.

---
+++ Week11Lab8/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week11Lab8
+{
+    public partial class Form1 : Form
+    {
+        List<double> list = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int count) && count > 0)
+            {
+                listBox1.Items.Clear();
+                await Task.Run(() =>
+                {
+                    CalculateFibonacci(count);
+
+                });
+                foreach (var item in list)
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid positive integer.");
+            }
+
+        }
+
+        private void CalculateFibonacci(int n)
+            {
+            Thread.Sleep(1000);
+            double a = 0;
+            double b = 1;
+            double c = 0;
+            for (int i = 0; i < n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                list.Add(c);
+            }
+
+        }
+
+    }
+
+}
